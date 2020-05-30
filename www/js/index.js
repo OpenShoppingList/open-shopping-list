b0fd97eb8a4a34f9e0a4738b6b51b46236045399
QuickWrite <playk3603@gmail.com>
2020-05-30 18:24:02 +0200

    Edit basic functionality in JS
    Co-Authored-By: Locxter <locxter@users.noreply.github.com>

--- www/js/index.js
+++ www/js/index.js
@@ -1,39 +1,97 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+let data = {
+    selectedList: 0,
+    lists: [
+        {
+            name: 'todo',
+            items: [
+                {
+                    name: 'item',
+                    checked: false,
+                    favorite: false
+                }
+            ]
+        }
+    ],
+    settings: [
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        
-    }
+    ]
 };
 
-app.initialize();
+var addListButtonValue = '';
+document.getElementById('add-list-button').addEventListener('click', function() {
+    if (document.getElementById('add-list-input').value && addListButtonValue !== document.getElementById('add-list-input').value) {
+        data.lists.push({
+            name: document.getElementById('add-list-input').value,
+            items: []
+        });
+        render(data.lists.length - 1, data.lists.length - 1);
+    }
+    addListButtonValue = document.getElementById('add-list-input').value
+});
+
+document.getElementById('add-list-input').addEventListener('change', function() {
+    if (document.getElementById('add-list-input').value) {
+        data.lists.push({
+            name: document.getElementById('add-list-input').value,
+            items: []
+        });
+        render(data.lists.length - 1, data.lists.length - 1);
+    }
+});
+
+function render(firstList, lastList) {
+    function renderList(i) {
+        if(document.getElementById('list-' + i)){
+            document.getElementById('list-' + i).remove();
+        }
+        let div = document.createElement('div');
+        div.id = 'list-' + i;
+        let h2 = document.createElement('h2');
+        let h2Text = document.createTextNode(data.lists[i].name);
+        let list = document.createElement('ul');
+        let input = document.createElement('input');
+        input.type = 'text';
+        input.className = 'add-item-input';
+        input.addEventListener('change', function() {
+            if (this.value) {
+                data.lists[this.parentNode.id.replace('list-', '')].items.push({
+                    name: this.value
+                });
+                render(this.parentNode.id.replace('list-', ''), this.parentNode.id.replace('list-', ''));
+            }
+        });
+        let button = document.createElement('button');
+        button.classsName = 'add-item-button';
+        button.addEventListener('click', function() {
+            if (this.parentNode.getElementsByClassName('add-item-input')[0].value) {
+                data.lists[this.parentNode.id.replace('list-', '')].items.push({
+                    name: this.parentNode.getElementsByClassName('add-item-input')[0].value
+                });
+                render(this.parentNode.id.replace('list-', ''), this.parentNode.id.replace('list-', ''));
+            }
+        });
+        let buttonText = document.createTextNode('Add list item');
+        h2.appendChild(h2Text);
+        for (let j = 0; j < data.lists[i].items.length; j++) {
+          let listItem = document.createElement('li');
+          let listItemText = document.createTextNode(data.lists[i].items[j].name);
+          listItem.appendChild(listItemText);
+          list.appendChild(listItem);
+        }
+        button.appendChild(buttonText);
+        div.appendChild(h2);
+        div.appendChild(list);
+        div.appendChild(input);
+        div.appendChild(button);
+        document.getElementById('lists').appendChild(div);
+    }
+    if(firstList >= 0 && firstList <= data.lists.length && lastList >= 0 && lastList <= data.lists.length && firstList <= lastList){
+        if(firstList === lastList) {
+            renderList(firstList);
+        } else {
+            for (let i = firstList; i < lastList; i++) {
+                renderList(i);
+            }
+        }
+    }
+}
